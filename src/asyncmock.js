81d2e174861aeb6e2462add0d8516feda864f689
alesiods <alesioadrover@gmail.com>
2024-09-12 14:55:50 -0300

    agregado de contador al detalle del producto

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const misProductos = [
-    { id: "1", nombre: "Yerba", precio: 10000, img: "../img/yerba.jpg", idCat: "lacteos" },
-    { id: "2", nombre: "Fideos", precio: 200, img: "../img/fideos.jpg", idCat:"lacteos" },
-    { id: "3", nombre: "Arroz", precio: 300, img: "../img/arroz.jpg", idCat:"almacen" }, 
-    { id: "4", nombre: "Aceite", precio: 900, img: "../img/aceite.jpg", idCat:"almacen" },
+    { id: "1", nombre: "Yerba", stock: 10, precio: 10000, img: "../img/yerba.jpg", idCat: "lacteos" },
+    { id: "2", nombre: "Fideos", stock: 12, precio: 200, img: "../img/fideos.jpg", idCat:"lacteos" },
+    { id: "3", nombre: "Arroz", stock: 5, precio: 300, img: "../img/arroz.jpg", idCat:"almacen" }, 
+    { id: "4", nombre: "Aceite", stock: 7, precio: 900, img: "../img/aceite.jpg", idCat:"almacen" },
 ]
 
 export const getProductos = () => {
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import "./ItemDetail.css"
+import Contador from '../Contador/Contador'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({id, nombre, precio, img}) => {
+
+const ItemDetail = ({id, nombre, precio, img, stock}) => {
+ // creamos un estado local con la cantidad de productos agregados
+  const [agregarCantidad, setAgregarCantidad] = useState(0)
+
+  //cremaos una funcion manejadora de la cantidad:
+
+  const manejadorCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    console.log("Productos agregador:" + cantidad)
+  }
 
   return (
     <div className='contenedorItem'>
@@ -10,6 +23,13 @@ const ItemDetail = ({id, nombre, precio, img}) => {
         <h3>ID: {id}</h3>
         <img src={img} alt={nombre} />
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, labore. Facilis commodi harum eligendi eum amet quia nesciunt asperiores. Quibusdam voluptates assumenda ipsam magni dignissimos totam ex culpa maiores atque?</p>
+
+        {
+          //Acá empleamos la logica de montaje y desmotaje del contador
+
+          agregarCantidad > 0 ? (<Link to="/cart"> Terminar Compra</Link>) : (<Contador inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/>)
+        }
+
     </div>
   )
 }
